eb49d045ddaf1a0e2eda62b08ece885b5b719b06
Subhan0004 <70345087+Subhan0004@users.noreply.github.com>
2021-04-01 11:59:06 +0400

    Init commit for repository

---
+++ Restaurant.Core/Domain/Entities/Customers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core.Domain.Entities
+{
+    public class Customers : BaseEntity 
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        string Address { get; set; }
+        string Phone { get; set; }
+        string Note { get; set; }
+        Users Creator { get; set; }
+        DateTime LastModifiedDate { get; set; }
+        bool IsDeleted { get; set; }
+    }
+}
---
+++ Restaurant.Core/Domain/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core.Domain.Entities
+{
+    public class Users : BaseEntity
+    {
+        int Id { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+        bool CanOperateCrm { get; set; }
+        Users Creator { get; set; }
+        DateTime LastModifiedDate { get; set; }
+        bool IsDeleted { get; set; }
+    }
+}
