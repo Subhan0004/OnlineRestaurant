1079290cf1a0896e25fe0a195238a72ac90f91aa
gun-el@outlook.com <gun-el@outlook.com>
2021-04-04 14:06:13 +0400

    Create SecurityHelper class

---
+++ Restaurant.Core/SecurityHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core
+{
+    public static class SecurityHelper
+    {
+        public static string ComputeSha256Hash(string rawDate)
+        {
+            using(SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawDate));
+
+                StringBuilder builder = new StringBuilder();
+
+                foreach(var t in bytes)
+                {
+                    builder.Append(t.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
