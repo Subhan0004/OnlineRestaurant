02bae1fcae4a1b00eb628a299c10ef7507dd29be
gun-el@outlook.com <gun-el@outlook.com>
2021-04-04 19:08:03 +0400

    Write code LoginViewModel

--- RestaurantApp/Command/LoginCommand.cs
+++ RestaurantApp/Command/LoginCommand.cs
@@ -1,16 +1,51 @@
-﻿using System;
+﻿using Restaurant.Core;
+using Restaurant.Core.Domain.Entities;
+using RestaurantApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace RestaurantApp.Command
 {
     public class LoginCommand : BaseCommand
     {
+        private readonly LoginViewModel viewModel;
+        public LoginCommand(LoginViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+        
         public override void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            User user = DB.UserRepository.Get(viewModel.Username);
+            if (user != null)
+            {
+                PasswordBox passwordBox = (PasswordBox)parameter;
+                string password = passwordBox.Password;
+                string passwordHash = SecurityHelper.ComputeSha256Hash(password);
+
+                if (user.Password == passwordHash)
+                {
+                    Kernel.CurrentUser = user;
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    viewModel.Window.Close();
+
+                }
+                else
+                {
+                    viewModel.ErrorVisibility = Visibility.Visible;
+                }
+
+            }
+            else
+            {
+                viewModel.ErrorVisibility = Visibility.Visible;
+            }
         }
     }
 }
