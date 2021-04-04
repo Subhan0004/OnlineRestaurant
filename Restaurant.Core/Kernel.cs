231dfc38539e4bd68f9a4b90ed3f01074a082742
gun-el@outlook.com <gun-el@outlook.com>
2021-04-04 13:54:10 +0400

    Create Kernel class

---
+++ Restaurant.Core/Kernel.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant.Core.Domain.Abstract;
+using Restaurant.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core
+{
+    public static class Kernel
+    {
+        public static User CurrentUser;
+        public static IUnitOfWork DB;
+    }
+}
