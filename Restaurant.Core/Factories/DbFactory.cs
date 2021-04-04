1a36e581cd095017bfa27de1f8a3b907f82930ce
gun-el@outlook.com <gun-el@outlook.com>
2021-04-04 13:47:47 +0400

    Supported DbFactory Factories

---
+++ Restaurant.Core/Factories/DbFactory.cs
@@ -0,0 +1,29 @@
+﻿using Restaurant.Core.DataAccess.SqlServer;
+using Restaurant.Core.Domain.Abstract;
+using Restaurant.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core.Factories
+{
+    public static class DbFactory
+    {
+        public static IUnitOfWork Create(ServerType serverType, string connectionString)
+        {
+            switch (serverType)
+            {
+                case ServerType.SqlServer:
+                    {
+                        return new SqlUnitOfWork(connectionString);
+                    }
+                default:
+                    {
+                        throw new NotSupportedException($"{serverType} not supported");
+                    }
+            }
+        }
+    }
+}
