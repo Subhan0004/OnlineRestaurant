f857ef66bc95545b29878a9a2d8a345805e4faa9
Subhan0004 <70345087+Subhan0004@users.noreply.github.com>
2021-04-05 13:43:13 +0400

    Update Dbsettings

--- RestaurantApp/Models/DBSettings.cs
+++ RestaurantApp/Models/DBSettings.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestaurantApp.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RestaurantApp.Models
 {
-    public class DBSettings
+    public class DbSettings
     {
+        public string ServerName { get; set; }
+        public string DbName { get; set; }
+        public bool IntegratedSecurity { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public const string CONFIGFOLDER = @"Config\";
+        public const string DBSETTINGS = @"Config\DbSettings.json";
+        private static DbSettings settings = new DbSettings();
+
+        private DbSettings()
+        {
+        }
+
+        public static DbSettings Get()
+        {
+            Directory.CreateDirectory(CONFIGFOLDER);
+
+            if (File.Exists(DBSETTINGS))
+            {
+                string json = File.ReadAllText(DBSETTINGS);
+                settings = JsonConvert.DeserializeObject<DbSettings>(json);
+            }
+            else
+            {
+                Helper.Log(new Exception("Server not found    \n"));
+                settings = new DbSettings()
+                {
+                    ServerName = "",
+                    DbName = "Online-RestaurantApp",
+                    IntegratedSecurity = true
+                };
+
+                string json = JsonConvert.SerializeObject(settings);
+                File.WriteAllText(DBSETTINGS, json);
+            }
+
+            return settings;
+        }
+
+        public string GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = settings.ServerName;
+            builder.InitialCatalog = settings.DbName;
+            builder.IntegratedSecurity = settings.IntegratedSecurity;
+
+            if (!builder.IntegratedSecurity)
+            {
+                builder.UserID = settings.Username;
+                //builder.Password = Helper.Decrypt(settings.CryptPassword);
+            }
+            return builder.ConnectionString;
+        }
+
+        //public void SaveConfig()
+        //{
+        //    string json = JsonConvert.SerializeObject(this);
+        //    File.WriteAllText(DBSETTINGS, json);
+        //}
     }
 }
