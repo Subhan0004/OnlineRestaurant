6f6442ff8f94b43911b828af8759fd682cde7697
Subhan0004 <70345087+Subhan0004@users.noreply.github.com>
2021-04-05 14:42:45 +0400

    Modified StartWindow.xaml

--- RestaurantApp/Views/StartWindow.xaml.cs
+++ RestaurantApp/Views/StartWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,43 @@ namespace RestaurantApp.Views
         public StartWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            CheckServer();
+        }
+
+        public /*async*/ void CheckServer()
+        {
+            //Kernel.DB = await DbFactory.CreateSqlDatabase(DbSettings.Get().GetConnectionString(), Server.EntityFramework);
+
+            DbSettings settins = DbSettings.Get();
+
+            //Kernel.DB = DbFactory.Create(ServerType.SqlServer);
+
+            //if (Kernel.DB == null)
+            //{
+            //    firstPanel.Visibility = Visibility.Hidden;
+            //    secondPanel.Visibility = Visibility.Visible;
+            //}
+            //else
+            //{
+            //    //await Task.Delay(2500);
+            //    LoginViewModel LoginVM = new LoginViewModel();
+            //    LoginPage login = new LoginPage(LoginVM);
+            //    Close();
+            //    login.ShowDialog();
+            //}
+        }
+
+        private void btnConfigClick(object sender, RoutedEventArgs e)
+        {
+            //SqlConfiguration config = new SqlConfiguration();
+            //config.ShowDialog();
+            //firstPanel.Visibility = Visibility.Visible;
+            //secondPanel.Visibility = Visibility.Hidden;
+            //CheckServer();
         }
     }
 }
