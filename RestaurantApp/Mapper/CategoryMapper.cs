faeda3247a195ff709666221159f3dd083bbf872
Subhan0004 <70345087+Subhan0004@users.noreply.github.com>
2021-04-22 16:22:05 +0400

    Create CategoryMapper and CategoryModel

---
+++ RestaurantApp/Mapper/CategoryMapper.cs
@@ -0,0 +1,34 @@
+﻿using Restaurant.Core.Domain.Entities;
+using RestaurantApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Mapper
+{
+    public class CategoryMapper : BaseMapper<Category, CategoryModel>
+    {
+        public override Category Map(CategoryModel categoryModel)
+        {
+            Category category = new Category();
+            category.Id = categoryModel.Id;
+            category.Name = categoryModel.Name;
+            category.Note = categoryModel.Note;
+           
+            return category;
+        }
+
+        public override CategoryModel Map(Category category)
+        {
+            CategoryModel categoryModel = new CategoryModel();
+
+            categoryModel.Id = category.Id;
+            categoryModel.Name = category.Name;
+            categoryModel.Note = category.Note;
+
+            return categoryModel;
+        }
+    }
+}
---
+++ RestaurantApp/Models/CategoryModel.cs
@@ -0,0 +1,52 @@
+﻿using RestaurantApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Models
+{
+    public class CategoryModel : BaseModel
+    {
+        public string Name { get; set; }
+        public string Note { get; set; }
+        public bool IsValid(out string message)
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                message = UIMessages.GetRequiredMessage("Ad");
+                return false;
+            }
+
+            if (Name.Length > 50)
+            {
+                message = UIMessages.GetLenghtMessage("Ad", 50);
+                return false;
+            }
+
+            if (!string.IsNullOrWhiteSpace(Note))
+            {
+                if (Note.Length > 250)
+                {
+                    message = UIMessages.GetLenghtMessage("Qeyd", 250);
+                    return false;
+                }
+            }
+
+            message = string.Empty;
+            return true;
+        }
+        public CategoryModel Clone()
+        {
+            CategoryModel cloneModel = new CategoryModel();
+
+            cloneModel.No = No;
+            cloneModel.Name = Name;
+            cloneModel.Id = Id;
+
+            return cloneModel;
+
+        }
+    }
+}
