d92505656a551e02a11e6d17d78ac245b5f8db32
gun-el@outlook.com <gun-el@outlook.com>
2021-05-15 16:31:45 +0400

    Create RestaurantAppWeb project

---
+++ RestaurantAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestaurantAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
