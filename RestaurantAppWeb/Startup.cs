f76fdd6ced7da2cb40511ebac07065833a64e699
gun-el@outlook.com <gun-el@outlook.com>
2021-05-15 18:20:54 +0400

    Delete RestaurantAppWeb project

--- RestaurantAppWeb/Startup.cs
+++ RestaurantAppWeb/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,6 +13,13 @@ namespace RestaurantAppWeb
 {
     public class Startup
     {
+
+        private readonly IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,14 +34,29 @@ namespace RestaurantAppWeb
                 app.UseDeveloperExceptionPage();
             }
 
+            else
+            {
+                // app.UseExceptionHandler("/Error/InternalError");
+                // app.UseStatusCodePagesWithRedirects("/Error/{0}");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Customer}/{action=Index}/{id?}");
+
+                //endpoints.MapAreaControllerRoute(
+                //    name: "areas",
+                //    areaName: "areas",
+                //    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
