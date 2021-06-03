398d62fac8ef5e1f48f8e20b1db1df306d932236
gun-el@outlook.com <gun-el@outlook.com>
2021-06-03 11:24:51 +0400

    upfate Meal Model

--- RestaurantAppWeb/Models/MealModel.cs
+++ RestaurantAppWeb/Models/MealModel.cs
@@ -9,18 +9,18 @@ namespace RestaurantAppWeb.Models
 {
     public class MealModel : BaseModel
     {
-        //[Required(ErrorMessage = "The name must be entered!")]
-        //[StringLength(100,ErrorMessage = "The name can't be longer than 10 characters")]
+        [Required(ErrorMessage = "The name must be entered!")]
+        [StringLength(100, ErrorMessage = "The name can't be longer than 10 characters")]
         public string Name { get; set; }
 
         public CategoryModel Category { get; set; }
 
         public decimal Price { get; set; }
-        
-        //[MaxLength(short.MaxValue, ErrorMessage ="The quantity can not more than 32767!")]
+
+        [Range(0.0, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
         public int Quantity { get; set; }
 
-        //[Required(ErrorMessage ="The note must be entered!")]
+        [Required(ErrorMessage = "The note must be entered!")]
         public string Note { get; set; }
 
         public List<SelectListItem> Categories;
